33ce0dcbdd733dc2f87d12fdcfe2db41804b1cd5
9utty <leegh4250@gmail.com>
2023-06-30 20:07:32 +0900

    feat: UserType 생성 및 User RTKQuery 생성

---
+++ redux/Api/User.ts
@@ -0,0 +1,20 @@
+import { UserDto } from "@/Types/UserTypes";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const userApi = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/" }),
+  endpoints: (builder) => ({
+    getUser: builder.query<UserDto, number>({
+      query: (id: number) => {
+        return {
+          url: `/users/${id}`,
+          method: "GET",
+        };
+      },
+      keepUnusedDataFor: 100,
+    }),
+  }),
+});
+
+export const { useGetUserQuery } = userApi;
