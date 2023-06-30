9ceef1f8bf480997a9cf71e94ba3a544707ff902
9utty <leegh4250@gmail.com>
2023-06-30 20:14:43 +0900

    feat: Store에 미들웨어 구성

--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import { rootReducer } from "./reducer";
+import { userApi } from "./Api/User";
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    rootReducers: rootReducer,
+    [userApi.reducerPath]: userApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware().concat(userApi.middleware);
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
