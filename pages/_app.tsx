dccd96f7119383fec1ae895415d665785cbddb9f
9utty <leegh4250@gmail.com>
2023-07-01 02:47:20 +0900

    feat: App 전체 타이틀 이미지 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <Head>
           <meta charSet="utf-8" />
           <title>42Manito</title>
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="https://user-images.githubusercontent.com/86397600/250159799-efa4ccf2-daeb-46f0-8679-655e4f2587df.png"
+          />
         </Head>
         <Component {...pageProps} />
       </Provider>
