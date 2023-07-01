3900209ee1066ad69b6e464e23825508d570e834
9utty <leegh4250@gmail.com>
2023-07-01 18:13:26 +0900

    생성: 프로필업데이트 파일 생성

---
+++ pages/ProfileUpdate.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AppLayout from "./global/AppLayout";
+import { Row } from "antd";
+
+const ProfileUpdate = () => {
+  return (
+    <AppLayout>
+      <Row>
+        <div></div>
+      </Row>
+    </AppLayout>
+  );
+};
+
+export default ProfileUpdate;
